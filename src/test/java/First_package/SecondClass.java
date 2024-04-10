26733b8edbc516fff293f653a749387ef3e7fac4
dukala <dukala0518@gmail.com>
2024-04-10 06:50:19 -0500

    third commit

--- src/test/java/First_package/SecondClass.java
+++ src/test/java/First_package/SecondClass.java
@@ -4,5 +4,6 @@ public class SecondClass {
     public static void main(String[] args) {
         System.out.println("Hello from second class");
         System.out.println("Hello again");
+        System.out.println("Hello any one is there? :-) ");
     }
 }
