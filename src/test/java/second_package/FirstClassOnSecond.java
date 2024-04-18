6d011c072aa30d51a5ffbfff919b6d113cfbd365
Dukala0518 <162185377+Dukala0518@users.noreply.github.com>
2024-04-18 10:48:36 -0500

    Update FirstClassOnSecond.java

--- src/test/java/second_package/FirstClassOnSecond.java
+++ src/test/java/second_package/FirstClassOnSecond.java
@@ -3,5 +3,9 @@ package second_package;
 public class FirstClassOnSecond {
     public static void main(String[] args) {
         System.out.println("FirstClassOnSecond");
+          System.out.println("It is from Git Hub");
+       
+       
+    
     }
 }
